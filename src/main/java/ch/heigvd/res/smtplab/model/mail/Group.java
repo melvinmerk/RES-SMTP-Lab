e52387cd7f8e39bf72ad4afdbaa680d8644fa8e1
gab-thr <gabrielle.thurnherr@gmail.com>
2021-05-01 23:58:36 +0200

    add group class

--- src/main/java/ch/heigvd/res/smtplab/model/mail/Group.java
+++ src/main/java/ch/heigvd/res/smtplab/model/mail/Group.java
@@ -1,10 +1,21 @@
 package ch.heigvd.res.smtplab.model.mail;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Group {
 
-    private List<Person> personList;
+    private List<Person> members;
 
+    public Group() {
+        this.members = new ArrayList<>();
+    }
 
+    public void addMember(Person person) {
+        members.add(person);
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
 }
