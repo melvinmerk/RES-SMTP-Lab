5819cfe85f41e811f62892e45958754ce7eb3674
Melvin Merk <merk.melvin@gmail.com>
2021-05-01 16:31:03 +0200

    Base config interface

--- src/main/java/ch/heigvd/res/smtplab/config/IConfigManager.java
+++ src/main/java/ch/heigvd/res/smtplab/config/IConfigManager.java
@@ -1,4 +1,11 @@
 package ch.heigvd.res.smtplab.config;
 
+import ch.heigvd.res.smtplab.model.mail.Person;
+
+import java.util.List;
+
 public interface IConfigManager {
+    List<String> loadMessageFromFile(String filename);
+    List<Person> loadVictimsFromFile(String filename);
+    void loadProperties();
 }
